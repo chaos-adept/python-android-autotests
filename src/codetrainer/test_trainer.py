548d0af1605120cf109d8823c34b4bc5a509a447
Denis Rykovanov <denis.rykovanov@mail.ru>
2022-04-24 22:17:54 +0300

    added simple compilation for java

--- src/codetrainer/test_trainer.py
+++ src/codetrainer/test_trainer.py
@@ -1,9 +1,35 @@
-from .trainer import add_one
+from sys import stderr
+from .trainer import generate, compile_sample, run
+import tempfile
 
+def test_codeGeneration():
+    # Given
+    expected = """
+    class HelloWorldApp {
+        public static void main(String[] args) {
+            String name = "Bob";
+            System.out.println("Hello World! " + name);
+        }
+    }
+"""
 
-def test_square():
     # When
-    subject = add_one(1)
+    code = generate("String name = \"Bob\";")
 
     # Then
-    assert subject == 2
+    assert expected == code
+
+
+def test_codeCompilation():
+    # Given
+    code_fragment = "int name = 1;"
+    working_dir = tempfile.TemporaryDirectory()
+    code, java_class, stdout, stderr = compile_sample(code_fragment, working_dir)
+    assert code == 0
+
+    # When
+    code, stdout, stderr = run(java_class, working_dir)
+    working_dir.cleanup()
+    # Then
+    assert code == 0, f'unexpected error code:{code}, stderr:{stderr}'
+    assert stdout == b"Hello World! 1\r\n"
--- src/codetrainer/trainer.py
+++ src/codetrainer/trainer.py
@@ -1,2 +1,36 @@
-def add_one(number):
-    return number + 1
+import os
+import subprocess
+
+from string import Template
+
+class_name = "HelloWorldApp"
+classTemplate = Template("""
+    class ${class_name} {
+        public static void main(String[] args) {
+            ${code_fragment}
+            System.out.println("Hello World! " + name);
+        }
+    }
+""")
+
+
+def generate(code_fragment):
+    return classTemplate.substitute(class_name=class_name, code_fragment=code_fragment)
+
+
+def compile_sample(code_fragment, working_dir):
+    java_file = os.path.join(working_dir.name, f"{class_name}.java")
+    java_class = class_name
+    with open(java_file, "w") as f:
+        f.write(generate(code_fragment=code_fragment))
+    cmd = [f"javac {java_file}"]
+    r = subprocess.run(cmd, capture_output=True, timeout=15, check=True)
+
+    return r.returncode, java_class, r.stdout, r.stderr
+
+
+def run(class_name, dir):
+    # todo try to use py4j https://www.py4j.org/getting_started.html#writing-the-java-program
+    cmd = ['java', f'-cp', dir.name, class_name]
+    r = subprocess.run(cmd, capture_output=True, timeout=15, check=True)
+    return r.returncode, r.stdout, r.stderr
