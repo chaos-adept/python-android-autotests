02cd4e2f51e40bb5435fe2f3f1744459d73ef48f
Denis Rykovanov <denis.rykovanov@mail.ru>
2022-04-25 13:49:36 +0300

    added logging

--- src/codetrainer/test_trainer.py
+++ src/codetrainer/test_trainer.py
@@ -1,6 +1,7 @@
-from sys import stderr
 from .trainer import generate, compile_sample, run
-import tempfile
+from ..logging_utils import config
+
+config.setup_logging()
 
 def test_generation():
     # Given
@@ -20,16 +21,15 @@ def test_generation():
     assert expected == code
 
 
-def test_compilation():
+def test_compilation(tmp_path):
     # Given
     code_fragment = "int name = 1;"
-    working_dir = tempfile.TemporaryDirectory()
-    code, java_class = compile_sample(code_fragment, working_dir)
+    code, java_class = compile_sample(code_fragment, tmp_path)
     assert code == 0
 
     # When
-    code, stdout, stderr = run(java_class, working_dir)
-    working_dir.cleanup()
+    code, stdout, stderr = run(java_class, tmp_path)
+
     # Then
     assert code == 0, f'unexpected error code:{code}, stderr:{stderr}'
     assert stdout == b"Hello World! 1\r\n"
--- src/codetrainer/trainer.py
+++ src/codetrainer/trainer.py
@@ -1,7 +1,9 @@
+import logging
 import os
 import subprocess
-
 from string import Template
+
+logger = logging.getLogger(__name__)
 
 class_name = "HelloWorldApp"
 classTemplate = Template("""
@@ -19,10 +21,13 @@ def generate(code_fragment):
 
 
 def compile_sample(code_fragment, working_dir):
-    java_file = os.path.join(working_dir.name, f"{class_name}.java")
+    logger.info("compile sample %s", code_fragment)
+    java_file = os.path.join(working_dir, f"{class_name}.java")
     java_class = class_name
+    generated_source = generate(code_fragment=code_fragment)
+    logger.debug("generated source %s", generated_source)
     with open(java_file, "w") as f:
-        f.write(generate(code_fragment=code_fragment))
+        f.write(generated_source)
     cmd = [f"javac", java_file]
     r = subprocess.run(cmd, capture_output=True, timeout=15, check=True)
 
@@ -31,6 +36,7 @@ def compile_sample(code_fragment, working_dir):
 
 def run(class_name, dir):
     # todo try to use py4j https://www.py4j.org/getting_started.html#writing-the-java-program
-    cmd = ['java', f'-cp', dir.name, class_name]
+    cmd = ['java', f'-cp', dir, class_name]
+    logger.info("run java with args %s", cmd)
     r = subprocess.run(cmd, capture_output=True, timeout=15, check=True)
     return r.returncode, r.stdout, r.stderr
